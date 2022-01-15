87dcc3b865be1a71882e0377d48370f763d66196
Syncxv <aruldevarackal1@gmail.com>
2022-01-16 02:46:53 +0530

    fixed notification running when they play spotify or change a song eh

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const { Plugin } = require("powercord/entities");
 const { FluxDispatcher, React, getModule } = require("powercord/webpack");
 const Settings = require("./components/Settings");
 class PINGWHENONLINE extends Plugin {
+    constructor(props) {
+        super(props);
+        this.idk = {};
+    }
     startPlugin() {
         powercord.api.settings.registerSettings(this.entityID, {
             category: this.entityID,
@@ -16,15 +20,20 @@ class PINGWHENONLINE extends Plugin {
     handlePresence(presence) {
         const people = this.settings.get("toNotify", []);
         people.forEach((id) => {
-            if (presence.user.id === id) {
-                console.log(presence.status);
+            if (presence.user.id === id && this.idk[id] !== presence.status) {
+                console.log(presence);
+                this.idk[id] = presence.status;
                 this.playAudio(this.settings.get("notificationSoundUrl", ""));
                 if (XenoLib && this.settings.get("useXeno", false)) {
                     if (!presence.user.username) {
                         const user = this.userStore.getUser(user.id);
-                        return XenoLib.Notifications.info(`${user.username} IS ${presence.status.toUpperCase()} WOAH`, { timeout: 5000 });
+                        return XenoLib.Notifications.info(`${user.username} IS ${presence.status.toUpperCase()} WOAH`, {
+                            timeout: this.settings.get("timeout", 5000),
+                        });
                     }
-                    return XenoLib.Notifications.info(`${presence.user.username} IS ${presence.status.toUpperCase()} :O`, { timeout: 5000 });
+                    return XenoLib.Notifications.info(`${presence.user.username} IS ${presence.status.toUpperCase()} :O`, {
+                        timeout: this.settings.get("timeout", 5000),
+                    });
                 }
 
                 powercord.api.notices.sendToast("user-notifer", {
@@ -54,7 +63,7 @@ class PINGWHENONLINE extends Plugin {
 
     pluginWillUnload() {
         powercord.api.settings.unregisterSettings(this.entityID);
-        FluxDispatcher.unsubscribe("PRESENCE_UPDATE", this.handlePresence);
+        FluxDispatcher.unsubscribe("PRESENCE_UPDATE", this.handlePresence.bind(this));
     }
 }
 
