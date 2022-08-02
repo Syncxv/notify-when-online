0afac9fd6c6d769614d8cbb94d133e1cec1b3743
Syncxv <aruldevarackal1@gmail.com>
2022-08-02 11:09:14 +0530

    when tf did i write this? WHY WAS IT SO BAD

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @type {import('../../../fake_node_modules/powercord/entities/').default} */
-const { Plugin } = require("powercord/entities");
-const { FluxDispatcher, React, getModule } = require("powercord/webpack");
-const Settings = require("./components/Settings");
+const { Plugin } = require('powercord/entities');
+const { FluxDispatcher, React, getModule } = require('powercord/webpack');
+const Settings = require('./components/Settings');
 class PINGWHENONLINE extends Plugin {
     constructor(props) {
         super(props);
@@ -10,47 +10,46 @@ class PINGWHENONLINE extends Plugin {
     startPlugin() {
         powercord.api.settings.registerSettings(this.entityID, {
             category: this.entityID,
-            label: "Notify Presence idk man",
+            label: 'Notify Presence idk man',
             render: Settings,
         });
-        FluxDispatcher.subscribe("PRESENCE_UPDATE", this.handlePresence.bind(this));
-        this.userStore = getModule(["getCurrentUser", "getUser"], false);
+        FluxDispatcher.subscribe('PRESENCE_UPDATES', this.handlePresence.bind(this));
+        this.userStore = getModule(['getCurrentUser', 'getUser'], false);
     }
 
-    handlePresence(presence) {
-        const people = this.settings.get("toNotify", []);
-        people.forEach((id) => {
-            if (presence.user.id === id && this.idk[id] !== presence.status) {
-                console.log(presence);
-                this.idk[id] = presence.status;
-                this.playAudio(this.settings.get("notificationSoundUrl", ""));
-                if (XenoLib && this.settings.get("useXeno", false)) {
-                    if (!presence.user.username) {
-                        const user = this.userStore.getUser(user.id);
-                        return XenoLib.Notifications.info(`${user.username} IS ${presence.status.toUpperCase()} WOAH`, {
-                            timeout: this.settings.get("timeout", 5000),
-                        });
-                    }
-                    return XenoLib.Notifications.info(`${presence.user.username} IS ${presence.status.toUpperCase()} :O`, {
-                        timeout: this.settings.get("timeout", 5000),
-                    });
-                }
+    handlePresence({ updates: [update] }) {
+        const people = this.settings.get('toNotify', []);
+        const userId = people.find((id) => update.user.id === id);
+        if (!userId || this.idk[update.user.id]?.status === update.status) return;
+        console.log(update);
+        this.idk[update.user.id] = update;
+        this.playAudio(this.settings.get('notificationSoundUrl', ''));
+        if (!update.user.username) {
+            const user = this.userStore.getUser(update.user.id);
+            return this.sendToast(user.username, update.status);
+        }
+        return this.sendToast(update.user.username, update.status);
+    }
 
-                powercord.api.notices.sendToast("user-notifer", {
-                    type: "success",
-                    header: "ONLINE NOTIFER",
-                    content: `${presence.user.username} IS ${presence.status.toUpperCase()} :O`,
-                    buttons: [
-                        {
-                            text: "Dismiss",
-                            color: "green",
-                            look: "outlined",
-                            onClick: () => powercord.api.notices.closeToast("user-notifer"),
-                        },
-                    ],
-                    timeout: 5e3,
-                });
-            }
+    sendToast(username, status) {
+        if (this.settings.get('useXeno', false)) {
+            return XenoLib.Notifications.info(`${username} IS ${status.toUpperCase()} WOAH`, {
+                timeout: this.settings.get('timeout', 5000),
+            });
+        }
+        return powercord.api.notices.sendToast('user-notifer', {
+            type: 'success',
+            header: 'ONLINE NOTIFER',
+            content: `${username} IS ${status.toUpperCase()} :O`,
+            buttons: [
+                {
+                    text: 'Dismiss',
+                    color: 'green',
+                    look: 'outlined',
+                    onClick: () => powercord.api.notices.closeToast('user-notifer'),
+                },
+            ],
+            timeout: 5e3,
         });
     }
 
@@ -63,7 +62,7 @@ class PINGWHENONLINE extends Plugin {
 
     pluginWillUnload() {
         powercord.api.settings.unregisterSettings(this.entityID);
-        FluxDispatcher.unsubscribe("PRESENCE_UPDATE", this.handlePresence.bind(this));
+        FluxDispatcher.unsubscribe('PRESENCE_UPDATES', this.handlePresence.bind(this));
     }
 }
 
